50f01d23b4ea888671ed3a2eec31649af7c709c7
Urooj Shah <60300832@udst.edu.qa>
2026-03-28 19:45:32 +0300

    adding frontend url to backend

--- backend_api/main.py
+++ backend_api/main.py
@@ -36,7 +36,7 @@ store = TempDocumentStore()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://mart-document-redaction-web.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
