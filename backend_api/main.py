016bb27bae8899e13f56cd74f128620c2be11adc
Urooj Shah <60300832@udst.edu.qa>
2026-03-28 20:40:28 +0300

    fixing url again in backend

--- backend_api/main.py
+++ backend_api/main.py
@@ -36,7 +36,7 @@ store = TempDocumentStore()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://mart-document-redaction-web.onrender.com"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://redactinator.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
